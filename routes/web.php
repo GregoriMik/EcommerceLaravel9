ee41757a27276d8d7c2c738c2737b5c0389db813
Grzegorz Mikina <gregorimikina@gmail.com>
2022-12-19 09:15:56 +0100

    Install and adapt breeze login system required

--- routes/web.php
+++ routes/web.php
@@ -1,4 +1,6 @@
 <?php
+
+use App\Http\Controllers\ProfileController;
 use App\Http\Livewire\HomeComponent;
 use App\Http\Livewire\CartComponent;
 use App\Http\Livewire\CheckoutComponent;
@@ -15,10 +17,23 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::get('/',HomeComponent::class)->name('home.index');
-Route::get('/shop',ShopComponent::class)->name('shop.index');
-Route::get('/checkout',CheckoutComponent::class)->name('checkout.index');
-Route::get('/cart',CartComponent::class)->name('cart.index');
+
 // Route::get('/', function () {
 //     return view('welcome');
 // });
+Route::get('/',HomeComponent::class)->name('home.index');
+Route::get('/shop',ShopComponent::class)->name('shop');
+Route::get('/checkout',CheckoutComponent::class)->name('shop.checkout');
+Route::get('/cart',CartComponent::class)->name('shop.cart');
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
